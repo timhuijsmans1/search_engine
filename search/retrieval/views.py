ca02f406df6d653579446e5b511350d568740258
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-01-20 15:17:50 +0000

    finished index view and search page template. Added results view to display query input

--- search/retrieval/views.py
+++ search/retrieval/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("hi page")
+    string = "Hi page"
+
+    context = {
+        'string': string,
+    }
+    return render(request, 'retrieval/index.html', context)
+
+def results(request):
+    form_data = value=request.POST
+
+    context = {
+        'category': form_data.get('category'),
+        'query': form_data.get('query'),
+        'date_start': form_data.get('date_start'),
+        'date_end': form_data.get('date_end'),
+    }
+    return render(request, 'retrieval/results.html', context)
