0baabb9e4a51de2a35618f64cb998ead67fd29d9
Vlad Matei <vlad.mateei@gmail.com>
2022-02-03 20:53:05 +0000

    Cosine similarity working with current index format

--- search/retrieval_development/vsm_model.py
+++ search/retrieval_development/vsm_model.py
@@ -12,29 +12,34 @@ from functions import extract_all_documents_term_appears_in
 
 class Vsm_model:
 
-    def compute_weight_term_document(self, term, document, positional_inverted_index, documents_appearing_in, N, doc_size):
+    def compute_weight_term_document(self, term, document, positional_inverted_index, documents_appearing_in, N,
+                                     doc_size):
         """
         Function to compute the term weight based on TFIDF term weighing
         """
-        if document not in positional_inverted_index[term][1]:
+        if document not in positional_inverted_index[term]:
             w_t_d = 0
         else:
-            tf = len(positional_inverted_index[term][1][document]) / (doc_size[document]) # how often the term appears in the current document /document size (for normalization)
+
+            doc_size_value = int(doc_size[document]) if doc_size[document] != 'NaN' else 1 # Extracting doc size -
+            # some elements have 'NaN' entry TO DO: ask Humzah to check
+            tf = len(positional_inverted_index[term][document]) / doc_size_value  # how often the term
+            # appears in the current document /document size (for normalization)
 
             df = len(documents_appearing_in[term])
-            idf = 1 + math.log(N/df) # Total Number of Docs / Numbers of docs with the term in them
+            idf = 1 + math.log(N / df)  # Total Number of Docs / Numbers of docs with the term in them
             w_t_d = tf * idf
         return w_t_d
-
 
     def compute_weight_term_query(self, term, query, N, documents_appearing_in):
         """
         Slide 21 - lecture 7 - red boxes are for query
         follows lnc.ltc schema -
         """
-        tf = query.count(term) / len(query)  # how many times the term appears in the query / query_length (for normalization)
+        tf = query.count(term) / len(
+            query)  # how many times the term appears in the query / query_length (for normalization)
         df = len(documents_appearing_in[term])
-        idf = 1 + math.log(N/df)  # IDF is the same regardless if we are looking for query or document weight term
+        idf = 1 + math.log(N / df)  # IDF is the same regardless if we are looking for query or document weight term
         w_t_q = tf * idf
         return w_t_q
 
@@ -58,18 +63,22 @@ class Vsm_model:
             document_vector = []
             query_vector = []
             for term in query:
-                w_t_d = self.compute_weight_term_document(term, document, pos_inverted_index, documents_appearing_in, N, doc_size)
+                w_t_d = self.compute_weight_term_document(term, document, pos_inverted_index, documents_appearing_in, N,
+                                                          doc_size)
                 w_t_q = self.compute_weight_term_query(term, query, N, documents_appearing_in)
                 document_vector.append(w_t_d)
                 query_vector.append(w_t_q)
                 score += w_t_d * w_t_q  # dot product (the summation of the terms in each vector multiplied)
             document_vector_magnitude = np.linalg.norm(document_vector)
             query_vector_magnitude = np.linalg.norm(query_vector)
-            score = score / (document_vector_magnitude*query_vector_magnitude)  # the product of the magnitudes of the two vectors
+            score = score / (
+                        document_vector_magnitude * query_vector_magnitude)  # the product of the magnitudes of the two vectors
+            score = float(score)
             document_scores[document] = score  # append the final score for each document
         sorted_document_scores = sorted(document_scores.items(), key=lambda x: x[1], reverse=True)
         sorted_document_scores = sorted_document_scores[:100]
         return sorted_document_scores
+
 
 if __name__ == '__main__':
     preprocessor = Preprocessing()
