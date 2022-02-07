d65931197032a88aee44268d968259c58c3f0173
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-02-07 14:42:38 +0000

    added document counters to the final returned index

--- indexing/index_read_write.py
+++ indexing/index_read_write.py
@@ -86,20 +86,21 @@ def delta_decoder(delta_encoded_inverted_list, word):
     return:
     inverted list in its original format {word: [document_count, [[doc_number, [positions]]]}
     """
-    dict_out = {word: {}}
 
     doc_count, doc_numbers = delta_encoded_inverted_list[word]
 
+    dict_out = {word: [doc_count, {}]}
+
     # add the first doc number manually
     doc_number, positions = doc_numbers[0]
-    dict_out[word][doc_number] = positions
+    dict_out[word][1][doc_number] = positions
 
     # loop over all but the first doc_numbers, first is not encoded
     for i in range(1, len(doc_numbers)):
         doc_number = sum([doc_tuple[0] for doc_tuple in doc_numbers[:i + 1]]) # calculate the doc number for each delta
         positions = doc_numbers[i][1]
-        dict_out[word][doc_number] = positions
-
+        dict_out[word][1][doc_number] = positions
+    print(dict_out)
     return dict_out
 
 def retrieval(word_list, word2byte, index_path):
@@ -139,4 +140,4 @@ if __name__ == "__main__":
 
     index = index_builder()
     word2byte = index_writer(index)
-    retrieval(['hello', 'edinburgh'], word2byte, INDEX_PATH)
+    retrieval(['hello', 'edinburgh', 'hhh'], word2byte, INDEX_PATH)
