4018668fb447288c996c9ae466ad98f00c00a879
Vlad Matei <vlad.mateei@gmail.com>
2022-02-10 13:34:44 +0000

    Trained lambdamart model on msft data and saving predictions to .txt

--- search/retrieval_development/lamda_rank/lamdarank.py
+++ search/retrieval_development/lamda_rank/lamdarank.py
@@ -3,7 +3,6 @@ import xgboost as xgb
 from datetime import datetime
 
 
-
 def get_D_matrix(df):
     """
     Function to create the DMatrix - internal data structure used by XGBoost,
@@ -25,7 +24,7 @@ def get_D_matrix(df):
     X = df.loc[:, 2:]  # extracts all features
     X.columns = ['x_' + str(x) for x in range(1, 137, 1)]
     X = X.apply(pd.to_numeric)
-    df_DMatrix = xgb.DMatrix(X, label=y) # Create the DMatrix which is pretty much a dataframe but optimised
+    df_DMatrix = xgb.DMatrix(X, label=y)  # Create the DMatrix which is pretty much a dataframe but optimised
     grp = grp.map(pd.to_numeric)
     df_DMatrix.set_group(grp_info_counts)
     print("Number of rows y label = " + str(len(grp)))
@@ -35,18 +34,27 @@ def get_D_matrix(df):
 
 # Reading the path as 'data/train.pkl' did not work (not found) but the absolute path seems to work
 
-print("Started Loading data ", datetime.now().strftime("%H:%M:%S"))
 train_df = pd.read_pickle(
     "/Users/vladmatei/PycharmProjects/TextTechnologiesDS/Search_engine/search/retrieval_development/data/train.pkl")
 test_df = pd.read_pickle(
     "/Users/vladmatei/PycharmProjects/TextTechnologiesDS/Search_engine/search/retrieval_development/data/test.pkl")
-print("Finished Loading data ", datetime.now().strftime("%H:%M:%S"))
 
-
-print("Started Creating train D_matrix ", datetime.now().strftime("%H:%M:%S"))
 train_data = get_D_matrix(train_df)
-print("Finished it", datetime.now().strftime("%H:%M:%S"))
-
-print("Started Creating test D_matrix ", datetime.now().strftime("%H:%M:%S"))
 test_data = get_D_matrix(test_df)
-print("Finished it", datetime.now().strftime("%H:%M:%S"))
+
+#### Using LambdaMart - combines LambdaRank and MART (Multiple Additive Regression Trees)
+#### On experimental data sets, LambdaMART has shown better results than LambdaRank and the original RankNet
+#### https://medium.com/@nikhilbd/intuitive-explanation-of-learning-to-rank-and-ranknet-lambdarank-and-lambdamart-fe1e17fac418
+
+params_lm6 = [('objective', 'rank:ndcg'),  # Use LambdaMART to perform list-wise ranking where NDCG is maximized
+              ('max_depth', 6), # Maximum depth of the tree - the higher the more likely to overfit
+              ('eta', 0.1),   # Step size shrinkage used in update to prevent overfitting
+              ('num_boost_round', 4),  # number of trees to build /
+              ('seed', 404)]  # seed for reproducibility
+
+model_lm6 = xgb.train(params_lm6, train_data)
+pred_lm6 = model_lm6.predict(test_data)
+
+pd.DataFrame(pred_lm6).to_csv("/Users/vladmatei/PycharmProjects/TextTechnologiesDS/Search_engine/search/retrieval_development/data/pred_lm6.txt",
+                              header=None,
+                              sep=" ")
