9d21f93895edfdbf5a72fabdf8e021d960e4f209
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-02-17 13:39:47 +0000

    implemented faster delta encoding algorithm. The algorithm is ran after first building the entire un-encoded index

--- indexing/database_population/db_updater.py
+++ indexing/database_population/db_updater.py
@@ -1,6 +1,6 @@
 def add_row(id, title, url, date, cursor, connection):
     values = f"'{id}', '{title}', '{url}', '{date}'"
-    insert = f'INSERT INTO retrieval_article (document_id, title, url, publication_date) VALUES ({values});'
+    insert = f'INSERT INTO retrieval_testArticle (document_id, title, url, publication_date) VALUES ({values});'
     cursor.execute(insert)
     connection.commit()
     return
