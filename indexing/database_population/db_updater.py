a87c275a64dbea69688f7018b8ae22c4ff69d4ed
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-03-06 13:53:44 +0000

    updated indexing script for wait times and db connection checks

--- indexing/database_population/db_updater.py
+++ indexing/database_population/db_updater.py
@@ -1,6 +1,14 @@
+import datetime
+import time
+
 def add_row(id, title, url, date, content, cursor, connection):
-    values = f"'{id}', '{title}', '{url}', '{date}', '{content}'"
-    insert = f'INSERT INTO retrieval_testArticle (document_id, title, url, publication_date, content) VALUES ({values});'
-    cursor.execute(insert)
-    connection.commit()
-    return
+    try:
+        values = f"'{id}', '{title}', '{url}', '{date}', '{content}'"
+        insert = f'INSERT INTO retrieval_testArticle (document_id, title, url, publication_date, content) VALUES ({values});'
+        cursor.execute(insert)
+        connection.commit()
+        return True
+    except Exception:
+        print("could not write to the database, sleeping 15 minutes")
+        time.sleep(900) 
+        return False
--- indexing/index_builder/index_build.py
+++ indexing/index_builder/index_build.py
@@ -1,6 +1,9 @@
 import sys
 import json
 import ast
+import signal
+import time
+import html
 
 from datetime import datetime
 from operator import sub
@@ -35,8 +38,10 @@ def index_extender(text_body, index, doc_number):
 
 def delta_encoder(index):
     encoded_index = {}
-
+    total_index_size = len(list(index.keys()))
+    count = 1
     for word in index:
+        print(f"{count/total_index_size}")
         occurences, inverted_list = index[word]
 
         # calculates the delta values and rebuilds the inverted list with deltas
@@ -60,6 +65,9 @@ def json_writer(data, path):
     with open(path, 'w') as f:
         json.dump(data, f)
     return
+
+def signal_handler(signum, frame):
+    raise Exception("Skipped a document because db writing took too long")
 
 def index_builder(content_file, 
                  stop_word_file_path,
@@ -86,7 +94,7 @@ def index_builder(content_file,
 
     with open(content_file, 'r') as f:
         next(f) # skip the header, delete for a file without header
-        while doc_id < 1000:
+        while doc_id < 600000:
             print(doc_id)
 
             line = f.readline().strip('\n')
@@ -100,43 +108,65 @@ def index_builder(content_file,
             # skip articles that can not be loaded into the db
             if len(title) >= 1000 or len(url) >= 1000:
                 continue
-
+            
             # whenever the date is provided in the wrong format, skip the iteration
             try:
                 # prepare data types for adding to db
-                publish_date_object = datetime.strptime(publish_date.strip(' 00:00:00'), '%Y-%m-%d')
-                title = title.replace('\'', '\'\'')
+                publish_date_object = datetime.strptime(publish_date.strip(' 00:00:00'), '%Y%m%d')
+                title = html.unescape(title.replace('\'', '\'\'')) # transform html entities to character
                 content = body.replace('\'', '\'\'')
             except:
-                print('Skipped a document')
+                print('Skipped a document because of the date format')
                 continue
             
             date2doc = date2doc_updater(publish_date, doc_id, date2doc)
 
             print("talking to db")
-            # add row to the database with the new info
-            add_row(doc_id, title, url, publish_date_object, content, cursor, connection)
-
-            # prepare text data for indexing
-            preprocessing = Preprocessing(stop_word_file_path)
-            pre_processed_article = preprocessing.apply_preprocessing(full_text)
-
-            # update size_dict
-            content_length = len(pre_processed_article)
-            sizes_dict[str(doc_id)] = int(content_length)
-
-            # update links dict
-            if len(links) > 0:
-                links = [link.strip() for link in ast.literal_eval(links)]
-                links_dict[str(doc_id)] = links
             
-            time_before = datetime.now()
-            # update index
-            inverted_index = index_extender(pre_processed_article, inverted_index, int(doc_id))
+            added_row = add_row(doc_id,
+                                title,
+                                url,
+                                publish_date_object,
+                                content,
+                                cursor,
+                                connection
+            )
+            if added_row == False:
+                try:
+                    connection, cursor = connect(database_config_path, database_cert_path)
+                except Exception:
+                    print("cannot connect to the db, continue to next article and try again")
+                continue
+
+            print("finished db writing")
+            
+            # if indexing takes longer than 1 min, skip document
+            signal.signal(signal.SIGALRM, signal_handler)
+            signal.alarm(60)
+            try:
+                # prepare text data for indexing
+                preprocessing = Preprocessing(stop_word_file_path)
+                pre_processed_article = preprocessing.apply_preprocessing(full_text)
+
+                # update size_dict
+                content_length = len(pre_processed_article)
+                sizes_dict[str(doc_id)] = int(content_length)
+
+                # update links dict
+                if len(links) > 0:
+                    links = [link.strip() for link in ast.literal_eval(links)]
+                    links_dict[str(doc_id)] = links
+                    time_before = datetime.now()
+                
+                # update index
+                inverted_index = index_extender(pre_processed_article, inverted_index, int(doc_id))
+                indexing_time = datetime.now() - time_before
+                print(f"Index updating took: {indexing_time}")
+                print('-------------')
+            except Exception:
+                print("Did not add document number to index")
+            signal.alarm(0)
             doc_id += 1
-            indexing_time = datetime.now() - time_before
-            print(indexing_time)
-            print('-------------')
 
     # write links and doc sizes to file
     json_writer(links_dict, link_dict_path)
@@ -144,7 +174,9 @@ def index_builder(content_file,
     json_writer(date2doc, date2doc_path)
 
     # delta encode inverted index for index writing
+    print("encoding index")
     encoded_index = delta_encoder(inverted_index)
+    print(f"finished encoding {sys.getsizeof(encoded_index)}")
     json_writer(encoded_index, index_path)
 
     return encoded_index
