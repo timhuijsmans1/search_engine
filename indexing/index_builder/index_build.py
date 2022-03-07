4763ca0653fda66d4de7e015cfa3e9513f22306b
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-03-07 13:02:25 +0000

    updated the indexing method to build separate indexes of smaller memory size and label them with the appropriate names

--- indexing/index_builder/index_build.py
+++ indexing/index_builder/index_build.py
@@ -1,9 +1,18 @@
-import sys
+from __future__ import print_function
+from sys import getsizeof, stderr
+from itertools import chain
+from collections import deque
+try:
+    from reprlib import repr
+except ImportError:
+    pass
+
 import json
 import ast
 import signal
 import time
 import html
+import os
 
 from datetime import datetime
 from operator import sub
@@ -12,28 +21,19 @@ from itertools import starmap, islice
 from database_population.db_connection import connect
 from database_population.db_updater import add_row
 from index_builder.helpers import Preprocessing
+from index_writer.index_writer import index_writer
 
 def index_extender(text_body, index, doc_number):
-
     for position, word in enumerate(text_body):
-
         if word in index:
-
-            last_doc_number = index[word][1][-1][0]
-            
-            # only add the position to the position list of the existing document number entry
-            if doc_number == last_doc_number: 
-                index[word][1][-1][1].append(position + 1) 
-            # add new doc number/position list to inverted list
+            if doc_number in index[word][1]:
+                index[word][1][doc_number].append(position + 1)
             else:
-                index[word][1].append([doc_number, [position + 1]])
-                # increment document count of word
-                index[word][0] += 1
+                index[word][1][doc_number] = [position + 1]
         
-        # build the initial list of doc/pos combos, no delta encoding on this iteration
         else:
-            index[word] = [1, [[doc_number, [position + 1]]]]
-    
+            index[word] = [1, {doc_number: [position + 1]}]
+
     return index
 
 def delta_encoder(index):
@@ -41,7 +41,7 @@ def delta_encoder(index):
     total_index_size = len(list(index.keys()))
     count = 1
     for word in index:
-        print(f"{count/total_index_size}")
+        print(f"{count} / {total_index_size}")
         occurences, inverted_list = index[word]
 
         # calculates the delta values and rebuilds the inverted list with deltas
@@ -52,7 +52,39 @@ def delta_encoder(index):
         encoded_inverted_list = [inverted_list[0], *deltas]
 
         encoded_index[word] = [occurences, encoded_inverted_list]
+        count += 1
     return encoded_index
+
+def total_size(o, handlers={}, verbose=False):
+    
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o), file=stderr)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
 
 def date2doc_updater(date, doc_id, date2doc):
     if date in date2doc: 
@@ -66,15 +98,16 @@ def json_writer(data, path):
         json.dump(data, f)
     return
 
-def signal_handler(signum, frame):
-    raise Exception("Skipped a document because db writing took too long")
+def partial_writer(inverted_index, index_path, partial_count):
+    sorted_index = dict(sorted(inverted_index.items()))
+    output_path = os.path.join(index_path, "index_" + str(partial_count) + ".json")
+    index_writer(sorted_index, output_path)
 
 def index_builder(content_file, 
                  stop_word_file_path,
                  database_config_path,
                  database_cert_path,
                  dict_size_path,
-                 link_dict_path,
                  doc_size_path,
                  index_path,
                  date2doc_path):
@@ -84,17 +117,17 @@ def index_builder(content_file,
                         {word: [document_count, [ [doc_delta, [positions]], [doc_delta, [positions]], ... ]}
     """
     inverted_index = {}
-    links_dict = {}
     sizes_dict = {}
     date2doc = {}
 
     connection, cursor = connect(database_config_path, database_cert_path)
 
     doc_id = 1
+    index_partial_count = 1
 
     with open(content_file, 'r') as f:
         next(f) # skip the header, delete for a file without header
-        while doc_id < 600000:
+        while True:
             print(doc_id)
 
             line = f.readline().strip('\n')
@@ -120,8 +153,6 @@ def index_builder(content_file,
                 continue
             
             date2doc = date2doc_updater(publish_date, doc_id, date2doc)
-
-            print("talking to db")
             
             added_row = add_row(doc_id,
                                 title,
@@ -138,45 +169,34 @@ def index_builder(content_file,
                     print("cannot connect to the db, continue to next article and try again")
                 continue
 
-            print("finished db writing")
-            
-            # if indexing takes longer than 1 min, skip document
-            signal.signal(signal.SIGALRM, signal_handler)
-            signal.alarm(60)
-            try:
-                # prepare text data for indexing
-                preprocessing = Preprocessing(stop_word_file_path)
-                pre_processed_article = preprocessing.apply_preprocessing(full_text)
+            # prepare text data for indexing
+            preprocessing = Preprocessing(stop_word_file_path)
+            pre_processed_article = preprocessing.apply_preprocessing(full_text)
 
-                # update size_dict
-                content_length = len(pre_processed_article)
-                sizes_dict[str(doc_id)] = int(content_length)
+            # update size_dict
+            content_length = len(pre_processed_article)
+            sizes_dict[str(doc_id)] = int(content_length)
+        
+            # update index
+            inverted_index = index_extender(pre_processed_article, inverted_index, int(doc_id))
 
-                # update links dict
-                if len(links) > 0:
-                    links = [link.strip() for link in ast.literal_eval(links)]
-                    links_dict[str(doc_id)] = links
-                    time_before = datetime.now()
-                
-                # update index
-                inverted_index = index_extender(pre_processed_article, inverted_index, int(doc_id))
-                indexing_time = datetime.now() - time_before
-                print(f"Index updating took: {indexing_time}")
-                print('-------------')
-            except Exception:
-                print("Did not add document number to index")
-            signal.alarm(0)
             doc_id += 1
 
-    # write links and doc sizes to file
-    json_writer(links_dict, link_dict_path)
+            # if index starts to exceed size, write it to file and restart
+            if doc_id % 10000 == 0:
+                inverted_size = total_size(inverted_index) / 1000000
+                date2_size = total_size(date2doc) / 1000000
+                sizes_size = total_size(sizes_dict) / 1000000
+                print(f"normal index size is {inverted_size} mb")
+                print(f"date2 size is {date2_size} mb")
+                print(f"sizes size index size is {sizes_size} mb")
+                if inverted_size > 2000:
+                    partial_writer(inverted_index, index_path, index_partial_count)
+                    # delete the index copy
+                    inverted_index = {}
+                    index_partial_count += 1
+
     json_writer(sizes_dict, doc_size_path)
     json_writer(date2doc, date2doc_path)
-
-    # delta encode inverted index for index writing
-    print("encoding index")
-    encoded_index = delta_encoder(inverted_index)
-    print(f"finished encoding {sys.getsizeof(encoded_index)}")
-    json_writer(encoded_index, index_path)
-
-    return encoded_index
+    
+    return
