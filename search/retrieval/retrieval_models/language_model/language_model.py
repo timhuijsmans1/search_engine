7193d280e0723ea98e6a0f94d119bb462d3cf22d
ghadi <gfouad9999@gmail.com>
2022-03-11 12:59:55 +0000

    Added full in depth deeply connected integrated highly detailed spectacular possibly working not quite sure search

--- search/retrieval/retrieval_models/language_model/language_model.py
+++ search/retrieval/retrieval_models/language_model/language_model.py
@@ -125,6 +125,7 @@ class Language_model:
         return document_scores
 
     def rank(self, query, mini_index, N, doc_sizes, l_tot, use_pitman_yor_process):
+
         documents_appearing_in = {}
         query_term_frequency = {}
         union_of_documents = []
@@ -132,15 +133,40 @@ class Language_model:
         for term in query:
             if term in mini_index.keys():
                 documents_appearing_in[term] = extract_all_documents_term_appears_in(mini_index[term][1])
-                query_term_frequency[term] = query.count(term)  # doing this here so we do not repeat it for each document
+                query_term_frequency[term] = query.count(
+                    term)  # doing this here so we do not repeat it for each document
 
         if documents_appearing_in:
             union_of_documents = sorted(reduce(set.union, map(set, documents_appearing_in.values())))
         length_collection = l_tot  # length of the collection in terms
         # g = self.g  # discounting parameter - as used in paper "Improvements to BM25 and Language Model examined - used for pyp implementation
         length_query = len(query)  # length of the query
-        sorted_scores = self.assign_scores(union_of_documents, query, mini_index, use_pitman_yor_process, query_term_frequency, length_collection, doc_sizes, abbv_bool)
-        return sorted_scores
+        document_scores = {}
+        for document in union_of_documents:
+            score = 0
+            for term in query:
+                if term in mini_index.keys():
+                    if use_pitman_yor_process:
+                        w_t_d = self.compute_weight_term_document_pyp(query_term_frequency[term], term, mini_index,
+                                                                      document, length_collection)
+                        score += w_t_d
+                    else:  # use lm-dirichlet smoothing
+                        self.miu = 1089  # Different value identified in the paper compared to those used when
+                        # pitman_yor_process is used
+                        w_t_d = self.compute_weight_term_document(query_term_frequency[term], term, mini_index,
+                                                                  document,
+                                                                  length_collection)
+                        score += w_t_d
+            L_d = doc_sizes[str(document)]
+            if use_pitman_yor_process:
+                dicsounted_l_d = max((L_d - self.g * (L_d ** self.g)), 0)
+                final_score = length_query * math.log(1 - (dicsounted_l_d / (L_d + self.miu))) + score
+            else:
+                final_score = length_query * math.log(self.miu / (L_d + self.miu)) + score
+            document_scores[document] = final_score
+        ## TO DO - Here find score with exactly equal values - or within 10% range - should be duplicates so only keep one
+
+        return document_scores
 
 
     # def assign_scores(self, union_of_documents, query, mini_index, use_pitman_yor_process, query_term_frequency, length_collection, doc_sizes, abbv_bool):
